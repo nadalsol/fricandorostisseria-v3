c86a8d80568cbdbbaa430069bd63c764bb45447f
Nadal Soler <nadalsoler@gmail.com>
2018-04-03 18:16:02 +0200

    feat: Reposition scroll towards category point (JS)

--- TODO.md
+++ TODO.md
@@ -8,7 +8,6 @@
 - [x] Add <title> attr. to `.svg` files.
 - [x] Refactor `.header` according to layout rules.
 - [x] Remove minima theme styles (CSS).
-- [x] Remove none used font faces (if any).
 - [x] Replace `.icon--small` and `.icon--large` icons and sizes (take a look at `_icons.scss`).
 - [x] Mobile styles (responsive).
 - [x] Clean up none-used CSS (commented code).
@@ -16,9 +15,8 @@
 - [x] Create hamburger menu (mobile), and behaviour (JS).
 - [x] Add `is-active` class to menu items: https://help.shopify.com/themes/liquid/filters/html-filters#script_tag
 - [x] Add `is-opaque` class to `header__nav`, when scroll > 0.
-- [x] Close `header-nav` on click `js-nav-main-link` item.
-- [ ] Animate scroll towards category point (JS).
-- [ ] Create new admin user (Rut): https://fricandorostisseria.admin.datocms.com/admin/users
+- [x] `Close `header-nav` on click `js-nav-main-link` item`.
+- [x] Reposition scroll towards category point (JS).
 
 ## Bugs
 
@@ -26,10 +24,10 @@
   with `background-size: contain` and truncate text (single line).
 - [ ] "Llámanos" is not visible in "call-us.png", because is white text over
   white background (small devices only).
-- [ ] favicon.ico is missing.
 
 ## Improvements (optional)
 
+- [ ] Replace old `favicon.ico` for the new one.
 - [ ] Minify, uglify, unify JS files: https://github.com/matthodan/jekyll-asset-pipeline
 - [ ] Replace jQuery for Vanilla JS code.
 - [ ] Remove minima theme: https://jekyllrb.com/docs/themes/#converting-gem-based-themes-to-regular-themes
@@ -39,3 +37,8 @@
 - [ ] Remove vendor-prefixes from `@mixin gradient-vertical` (if necessary).
 - [ ] Check printed version.
 - [ ] 404 page styles.
+
+## Discarded
+
+- Remove none used font faces (if any).
+- Create new DatoCMS admin user (Rut).
--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,7 +1,9 @@
-/**
- * The document ready event executes already when the HTML-Document is loaded
- * and the DOM is ready, even if all the graphics haven't loaded yet.
- */
+////////////////////////////////////////////////////////////////////////////////
+//                                                                            //
+// The document ready event executes already when the HTML-Document is loaded //
+// and the DOM is ready, even if all the graphics haven't loaded yet.         //
+//                                                                            //
+////////////////////////////////////////////////////////////////////////////////
 $(document).ready(function() {
 
   var headerHamburger = $('#header-hamburger'),
@@ -55,10 +57,40 @@ $(document).ready(function() {
 });
 
 
-/**
- * The window load event executes a bit later when the complete page
- * is fully loaded, including all frames, objects and images.
- */
-// $(window).load(function() {
-  // Your code here...
-// });
+////////////////////////////////////////////////////////////////////////////////
+//                                                                            //
+// The window load event executes a bit later when the complete page          //
+// is fully loaded, including all frames, objects and images.                 //
+//                                                                            //
+////////////////////////////////////////////////////////////////////////////////
+$(window).on('load', function() {
+
+  var header = $('#header'),
+      jsNavMainLink = $('.js-nav-main-link');
+
+  /**
+   * Repositioner
+   *
+   * Prevents fixed `header` overlapping of target elements, repositioning
+   * the current scroll. Useful to link to specific sections of the page.
+   *
+   */
+  function computeBodyOffsetTop() {
+    var headerHeight = header.outerHeight(true);
+        spMedium = 25; // same as `$sp-medium` defined in `_variables.scss`
+    return headerHeight + spMedium;
+  }
+
+  // Repositioning via link click
+  jsNavMainLink.click(function() {
+    var bodyOffsetTop = computeBodyOffsetTop(),
+        theTarget = $(this).attr('href'),
+        theTargetTopPosition = $(theTarget).offset().top,
+        theTargetTopPositionOffset = theTargetTopPosition - bodyOffsetTop;
+    // scroll to target
+    $('body, html').animate({
+      scrollTop: theTargetTopPositionOffset
+    }, 250);
+  });
+
+});
